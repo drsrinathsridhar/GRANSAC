a8f80d7993ae03432af7345bc6797e88877a8971
drsrinathsridhar <ssridhar@mpi-inf.mpg.de>
2017-06-23 18:12:20 +0200

    Changed INFO to cout. Merged pull request and unit tested.

--- examples/LineFittingSample.cpp
+++ examples/LineFittingSample.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <cmath>
 #include <random>
 
@@ -9,100 +8,100 @@
 
 GRANSAC::VPFloat Slope(int x0, int y0, int x1, int y1)
 {
-    return (GRANSAC::VPFloat)(y1-y0)/(x1-x0);
+	return (GRANSAC::VPFloat)(y1 - y0) / (x1 - x0);
 }
 
 void DrawFullLine(cv::Mat& img, cv::Point a, cv::Point b, cv::Scalar color, int LineWidth)
 {
-    GRANSAC::VPFloat slope = Slope(a.x, a.y, b.x, b.y);
+	GRANSAC::VPFloat slope = Slope(a.x, a.y, b.x, b.y);
 
-    cv::Point p(0,0), q(img.cols, img.rows);
+	cv::Point p(0, 0), q(img.cols, img.rows);
 
-    p.y = -(a.x - p.x) * slope + a.y;
-    q.y = -(b.x - q.x) * slope + b.y;
+	p.y = -(a.x - p.x) * slope + a.y;
+	q.y = -(b.x - q.x) * slope + b.y;
 
-    cv::line(img, p, q, color, LineWidth, 8, 0);
+	cv::line(img, p, q, color, LineWidth, 8, 0);
 }
 
 int main(int argc, char * argv[])
 {
-    if(argc != 1 && argc != 3)
-    {
-	std::cout << "[ USAGE ]: " << argv[0] << " [<Image Size> = 1000] [<nPoints> = 500]" << std::endl;
-	return -1;
-    }
-
-    int Side = 1000;
-    int nPoints = 500;
-    if(argc == 3)
-    {
-	Side = std::atoi(argv[1]);
-	nPoints = std::atoi(argv[2]);
-    }
-
-    cv::Mat Canvas(Side, Side, CV_8UC3);
-    Canvas.setTo(255);
-
-    // Randomly generate points in a 2D plane roughly aligned in a line for testing
-    std::random_device SeedDevice;
-    std::mt19937 RNG = std::mt19937(SeedDevice());
-
-    std::uniform_int_distribution<int> UniDist(0, Side-1); // [Incl, Incl]
-    int Perturb = 25;
-    std::normal_distribution<GRANSAC::VPFloat> PerturbDist(0, Perturb);
-
-    std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> CandPoints;
-    for(int i = 0; i < nPoints; ++i)
-    {
-	int Diag = UniDist(RNG);
-	cv::Point Pt(floor(Diag + PerturbDist(RNG)), floor(Diag + PerturbDist(RNG)));
-	cv::circle(Canvas, Pt, floor(Side / 100), cv::Scalar(0, 0, 0), -1);
-
-	std::shared_ptr<GRANSAC::AbstractParameter> CandPt = std::make_shared<Point2D>(Pt.x, Pt.y);
-	CandPoints.push_back(CandPt);
-    }
-
-    GRANSAC::RANSAC<Line2DModel, 2> Estimator;
-    Estimator.Initialize(20, 100); // Threshold, iterations
-    int start = cv::getTickCount();
-    Estimator.Estimate(CandPoints);
-    int end = cv::getTickCount();
-    std::cout << "RANSAC took: " << GRANSAC::VPFloat(end-start) / GRANSAC::VPFloat(cv::getTickFrequency()) * 1000.0 << " ms." << std::endl;
-
-    auto BestInliers = Estimator.GetBestInliers();
-    if(BestInliers.size() > 0)
-    {
-	for(auto& Inlier : BestInliers)
+	if (argc != 1 && argc != 3)
 	{
-	    auto RPt = std::dynamic_pointer_cast<Point2D>(Inlier);
-	    cv::Point Pt(floor(RPt->m_Point2D[0]), floor(RPt->m_Point2D[1]));
-	    cv::circle(Canvas, Pt, floor(Side / 100), cv::Scalar(0, 255, 0), -1);
+		std::cout << "[ USAGE ]: " << argv[0] << " [<Image Size> = 1000] [<nPoints> = 500]" << std::endl;
+		return -1;
 	}
-    }
 
-    auto BestLine = Estimator.GetBestModel();
-    if(BestLine)
-    {
-	auto BestLinePt1 = std::dynamic_pointer_cast<Point2D>(BestLine->GetModelParams()[0]);
-	auto BestLinePt2 = std::dynamic_pointer_cast<Point2D>(BestLine->GetModelParams()[1]);
-	if(BestLinePt1 && BestLinePt2)
+	int Side = 1000;
+	int nPoints = 500;
+	if (argc == 3)
 	{
-	    cv::Point Pt1(BestLinePt1->m_Point2D[0], BestLinePt1->m_Point2D[1]);
-	    cv::Point Pt2(BestLinePt2->m_Point2D[0], BestLinePt2->m_Point2D[1]);
-	    DrawFullLine(Canvas, Pt1, Pt2, cv::Scalar(0, 0, 255), 2);
+		Side = std::atoi(argv[1]);
+		nPoints = std::atoi(argv[2]);
 	}
-    }
 
-    while(true)
-    {
-	cv::imshow("RANSAC Example", Canvas);
+	cv::Mat Canvas(Side, Side, CV_8UC3);
+	Canvas.setTo(255);
 
-	char Key = cv::waitKey(1);
-	if(Key == 27)
-	    return 0;
-	if(Key == ' ')
-	    cv::imwrite("LineFitting.png", Canvas);
-    }
+	// Randomly generate points in a 2D plane roughly aligned in a line for testing
+	std::random_device SeedDevice;
+	std::mt19937 RNG = std::mt19937(SeedDevice());
 
-    return 0;
+	std::uniform_int_distribution<int> UniDist(0, Side - 1); // [Incl, Incl]
+	int Perturb = 25;
+	std::normal_distribution<GRANSAC::VPFloat> PerturbDist(0, Perturb);
+
+	std::vector<std::shared_ptr<GRANSAC::AbstractParameter>> CandPoints;
+	for (int i = 0; i < nPoints; ++i)
+	{
+		int Diag = UniDist(RNG);
+		cv::Point Pt(floor(Diag + PerturbDist(RNG)), floor(Diag + PerturbDist(RNG)));
+		cv::circle(Canvas, Pt, floor(Side / 100), cv::Scalar(0, 0, 0), -1);
+
+		std::shared_ptr<GRANSAC::AbstractParameter> CandPt = std::make_shared<Point2D>(Pt.x, Pt.y);
+		CandPoints.push_back(CandPt);
+	}
+
+	GRANSAC::RANSAC<Line2DModel, 2> Estimator;
+	Estimator.Initialize(20, 100); // Threshold, iterations
+	int start = cv::getTickCount();
+	Estimator.Estimate(CandPoints);
+	int end = cv::getTickCount();
+	std::cout << "RANSAC took: " << GRANSAC::VPFloat(end - start) / GRANSAC::VPFloat(cv::getTickFrequency()) * 1000.0 << " ms." << std::endl;
+
+	auto BestInliers = Estimator.GetBestInliers();
+	if (BestInliers.size() > 0)
+	{
+		for (auto& Inlier : BestInliers)
+		{
+			auto RPt = std::dynamic_pointer_cast<Point2D>(Inlier);
+			cv::Point Pt(floor(RPt->m_Point2D[0]), floor(RPt->m_Point2D[1]));
+			cv::circle(Canvas, Pt, floor(Side / 100), cv::Scalar(0, 255, 0), -1);
+		}
+	}
+
+	auto BestLine = Estimator.GetBestModel();
+	if (BestLine)
+	{
+		auto BestLinePt1 = std::dynamic_pointer_cast<Point2D>(BestLine->GetModelParams()[0]);
+		auto BestLinePt2 = std::dynamic_pointer_cast<Point2D>(BestLine->GetModelParams()[1]);
+		if (BestLinePt1 && BestLinePt2)
+		{
+			cv::Point Pt1(BestLinePt1->m_Point2D[0], BestLinePt1->m_Point2D[1]);
+			cv::Point Pt2(BestLinePt2->m_Point2D[0], BestLinePt2->m_Point2D[1]);
+			DrawFullLine(Canvas, Pt1, Pt2, cv::Scalar(0, 0, 255), 2);
+		}
+	}
+
+	while (true)
+	{
+		cv::imshow("RANSAC Example", Canvas);
+
+		char Key = cv::waitKey(1);
+		if (Key == 27)
+			return 0;
+		if (Key == ' ')
+			cv::imwrite("LineFitting.png", Canvas);
+	}
+
+	return 0;
 }
--- include/GRANSAC.hpp
+++ include/GRANSAC.hpp
@@ -13,116 +13,116 @@
 
 namespace GRANSAC
 {
-    // T - AbstractModel
-    template <class T, int t_NumParams>
-    class RANSAC
-    {
-    private:
-	std::vector<std::shared_ptr<AbstractParameter>> m_Data; // All the data
-
-	std::vector<std::shared_ptr<T>> m_SampledModels; // Vector of all sampled models
-	std::shared_ptr<T> m_BestModel; // Pointer to the best model, valid only after Estimate() is called
-	std::vector<std::shared_ptr<AbstractParameter>> m_BestInliers;
-
-	int m_MaxIterations; // Number of iterations before termination
-	VPFloat m_Threshold; // The threshold for computing model consensus
-	VPFloat m_BestModelScore; // The score of the best model
-	int m_BestModelIdx;
-
-	std::vector<std::mt19937> m_RandEngines; // Mersenne twister high quality RNG that support *OpenMP* multi-threading
-
-    public:
-	RANSAC(void)
-        {
-	    int nThreads = std::max(1, omp_get_max_threads());
-            std::cerr << "[ INFO ]: Maximum usable threads: " << nThreads << std::endl;
-	    for(int i = 0; i < nThreads; ++i)
-	    {
-		std::random_device SeedDevice;
-		m_RandEngines.push_back(std::mt19937(SeedDevice()));
-	    }
-
-	    Reset();
-	};
-
-	virtual ~RANSAC(void) {};
-
-	void Reset(void)
+	// T - AbstractModel
+	template <class T, int t_NumParams>
+	class RANSAC
 	{
-	    // Clear sampled models, etc. and prepare for next call. Reset RANSAC estimator state
-	    m_Data.clear();
-	    m_SampledModels.clear();
+	private:
+		std::vector<std::shared_ptr<AbstractParameter>> m_Data; // All the data
 
-	    m_BestModelIdx = -1;
-	    m_BestModelScore = 0.0;
-	};
+		std::vector<std::shared_ptr<T>> m_SampledModels; // Vector of all sampled models
+		std::shared_ptr<T> m_BestModel; // Pointer to the best model, valid only after Estimate() is called
+		std::vector<std::shared_ptr<AbstractParameter>> m_BestInliers;
 
-	void Initialize(VPFloat Threshold, int MaxIterations = 1000)
-	{
-	    m_Threshold = Threshold;
-	    m_MaxIterations = MaxIterations;
-	};
+		int m_MaxIterations; // Number of iterations before termination
+		VPFloat m_Threshold; // The threshold for computing model consensus
+		VPFloat m_BestModelScore; // The score of the best model
+		int m_BestModelIdx;
 
-	std::shared_ptr<T> GetBestModel(void) { return m_BestModel; };
-	const std::vector<std::shared_ptr<AbstractParameter>>& GetBestInliers(void) { return m_BestInliers; };
+		std::vector<std::mt19937> m_RandEngines; // Mersenne twister high quality RNG that support *OpenMP* multi-threading
 
-        bool Estimate(const std::vector<std::shared_ptr<AbstractParameter>> &Data)
-	{
-	    if(Data.size() <= t_NumParams)
-	    {
-                std::cerr << "[ WARN ]: RANSAC - Number of data points is too less. Not doing anything." << std::endl;
-		return false;
-	    }
-
-	    m_Data = Data;
-	    int DataSize = m_Data.size();
-	    std::uniform_int_distribution<int> UniDist(0, int(DataSize-1)); // Both inclusive
-
-	    std::vector<VPFloat> InlierFractionAccum(m_MaxIterations);
-	    std::vector<std::vector<std::shared_ptr<AbstractParameter>>> InliersAccum(m_MaxIterations);
-	    m_SampledModels.resize(m_MaxIterations);
-	
-	    int nThreads = std::max(1, omp_get_max_threads());
-	    omp_set_dynamic(0); // Explicitly disable dynamic teams
-	    omp_set_num_threads(nThreads);
-#pragma omp parallel for
-	    for(int i = 0; i < m_MaxIterations; ++i)
-	    {
-		// Select t_NumParams random samples
-		std::vector<std::shared_ptr<AbstractParameter>> RandomSamples(t_NumParams);
-		std::vector<std::shared_ptr<AbstractParameter>> RemainderSamples = m_Data; // Without the chosen random samples
-
-		std::shuffle(RemainderSamples.begin(), RemainderSamples.end(), m_RandEngines[omp_get_thread_num()]); // To avoid picking the same element more than once
-		std::copy(RemainderSamples.begin(), RemainderSamples.begin() + t_NumParams, RandomSamples.begin());
-		RemainderSamples.erase(RemainderSamples.begin(), RemainderSamples.begin() + t_NumParams);
-
-		std::shared_ptr<T> RandomModel = std::make_shared<T>(RandomSamples);
-
-		// Check if the sampled model is the best so far
-		std::pair<VPFloat, std::vector<std::shared_ptr<AbstractParameter>>> EvalPair = RandomModel->Evaluate(RemainderSamples, m_Threshold);
-		InlierFractionAccum[i] = EvalPair.first;
-		InliersAccum[i] = EvalPair.second;
-
-		// Push back into history. Could be removed later
-		m_SampledModels[i] = RandomModel;
-	    }
-
-	    for(int i = 0; i < m_MaxIterations; ++i)
-	    {
-		if(InlierFractionAccum[i] > m_BestModelScore)
+	public:
+		RANSAC(void)
 		{
-		    m_BestModelScore = InlierFractionAccum[i];
-		    m_BestModelIdx = m_SampledModels.size() - 1; 
-		    m_BestModel = m_SampledModels[i];
-		    m_BestInliers = InliersAccum[i];
-		}
-	    }
+			int nThreads = std::max(1, omp_get_max_threads());
+			std::cout << "[ INFO ]: Maximum usable threads: " << nThreads << std::endl;
+			for (int i = 0; i < nThreads; ++i)
+			{
+				std::random_device SeedDevice;
+				m_RandEngines.push_back(std::mt19937(SeedDevice()));
+			}
 
-            // std::cerr << "BestInlierFraction: " << m_BestModelScore << std::endl;
+			Reset();
+		};
 
-	    Reset();
+		virtual ~RANSAC(void) {};
 
-	    return true;
+		void Reset(void)
+		{
+			// Clear sampled models, etc. and prepare for next call. Reset RANSAC estimator state
+			m_Data.clear();
+			m_SampledModels.clear();
+
+			m_BestModelIdx = -1;
+			m_BestModelScore = 0.0;
+		};
+
+		void Initialize(VPFloat Threshold, int MaxIterations = 1000)
+		{
+			m_Threshold = Threshold;
+			m_MaxIterations = MaxIterations;
+		};
+
+		std::shared_ptr<T> GetBestModel(void) { return m_BestModel; };
+		const std::vector<std::shared_ptr<AbstractParameter>>& GetBestInliers(void) { return m_BestInliers; };
+
+		bool Estimate(const std::vector<std::shared_ptr<AbstractParameter>> &Data)
+		{
+			if (Data.size() <= t_NumParams)
+			{
+				std::cerr << "[ WARN ]: RANSAC - Number of data points is too less. Not doing anything." << std::endl;
+				return false;
+			}
+
+			m_Data = Data;
+			int DataSize = m_Data.size();
+			std::uniform_int_distribution<int> UniDist(0, int(DataSize - 1)); // Both inclusive
+
+			std::vector<VPFloat> InlierFractionAccum(m_MaxIterations);
+			std::vector<std::vector<std::shared_ptr<AbstractParameter>>> InliersAccum(m_MaxIterations);
+			m_SampledModels.resize(m_MaxIterations);
+
+			int nThreads = std::max(1, omp_get_max_threads());
+			omp_set_dynamic(0); // Explicitly disable dynamic teams
+			omp_set_num_threads(nThreads);
+#pragma omp parallel for
+			for (int i = 0; i < m_MaxIterations; ++i)
+			{
+				// Select t_NumParams random samples
+				std::vector<std::shared_ptr<AbstractParameter>> RandomSamples(t_NumParams);
+				std::vector<std::shared_ptr<AbstractParameter>> RemainderSamples = m_Data; // Without the chosen random samples
+
+				std::shuffle(RemainderSamples.begin(), RemainderSamples.end(), m_RandEngines[omp_get_thread_num()]); // To avoid picking the same element more than once
+				std::copy(RemainderSamples.begin(), RemainderSamples.begin() + t_NumParams, RandomSamples.begin());
+				RemainderSamples.erase(RemainderSamples.begin(), RemainderSamples.begin() + t_NumParams);
+
+				std::shared_ptr<T> RandomModel = std::make_shared<T>(RandomSamples);
+
+				// Check if the sampled model is the best so far
+				std::pair<VPFloat, std::vector<std::shared_ptr<AbstractParameter>>> EvalPair = RandomModel->Evaluate(RemainderSamples, m_Threshold);
+				InlierFractionAccum[i] = EvalPair.first;
+				InliersAccum[i] = EvalPair.second;
+
+				// Push back into history. Could be removed later
+				m_SampledModels[i] = RandomModel;
+			}
+
+			for (int i = 0; i < m_MaxIterations; ++i)
+			{
+				if (InlierFractionAccum[i] > m_BestModelScore)
+				{
+					m_BestModelScore = InlierFractionAccum[i];
+					m_BestModelIdx = m_SampledModels.size() - 1;
+					m_BestModel = m_SampledModels[i];
+					m_BestInliers = InliersAccum[i];
+				}
+			}
+
+			// std::cerr << "BestInlierFraction: " << m_BestModelScore << std::endl;
+
+			Reset();
+
+			return true;
+		};
 	};
-    };
 } // namespace GRANSAC
